fa50cd730a170bedf3c60dd6eef8a49ea8cc22d5
Sy Le <syle@linkedin.com>
2018-05-25 14:20:37 -0700

    clean up

--- app/helpers/placeholder-url.js
+++ app/helpers/placeholder-url.js
@@ -2,9 +2,19 @@ import { helper } from '@ember/component/helper';
 
 const IMAGE_URL = 'http://placekitten.com';
 
-export function placeholderUrl(params, hash) {
-  const w = Math.max(hash.w || 100, 10);
-  const h = Math.max(hash.h || 120, 10);
+
+// old
+// export function placeholderUrl(_params, hash) {
+//   const w = Math.max(hash.w || 100, 10);
+//   const h = Math.max(hash.h || 120, 10);
+
+//   return `${IMAGE_URL}/${w}/${h}`;
+// }
+
+
+export function placeholderUrl(_params, {w = 100, h = 120}) {
+  w = Math.max(10, w);
+  h = Math.max(10, h);
 
   return `${IMAGE_URL}/${w}/${h}`;
 }
