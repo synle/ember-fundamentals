176b0f8e4116dca4fd14285834c8588e3913afb3
Sy Le <syle@linkedin.com>
2018-05-29 15:30:33 -0700

    clean up with async await...

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,14 +2,12 @@ import Route from '@ember/routing/route';
 import { ALL_COURSES as courses } from '../controllers/application';
 
 export default Route.extend({
-    model() {
-        return fetch('https://mikenorth-api-stage.herokuapp.com/api/v1/courses')
-            .then(response => response.json())
-            .then(({data}) => {
-                return {
-                    courses: data.map(course => Object.assign({id: course.id}, course.attributes))
-                }
-            })
+    async model() {
+        const response = await fetch('https://mikenorth-api-stage.herokuapp.com/api/v1/courses');
+        const { data: responseData } = await response.json();
+        return {
+            courses: responseData.map(course => Object.assign({id: course.id}, course.attributes))
+        }
     },
     model2() {
         return Promise.resolve({
