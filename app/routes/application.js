cd796c53892ec7c63973f58e990568e06ecd59c2
Sy Le <syle@linkedin.com>
2018-05-29 15:27:14 -0700

    works for both models...

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,9 +1,19 @@
 import Route from '@ember/routing/route';
+import { ALL_COURSES as courses } from '../controllers/application';
 
 export default Route.extend({
     model() {
         return fetch('https://mikenorth-api-stage.herokuapp.com/api/v1/courses')
             .then(response => response.json())
-            .then(({data}) => ({courses: data}))
+            .then(({data}) => {
+                return {
+                    courses: data.map(course => Object.assign({id: course.id}, course.attributes))
+                }
+            })
+    },
+    model2() {
+        return Promise.resolve({
+            courses: courses.map((course, id) => Object.assign({id: '' + id}, course))
+        });
     }
 });
--- app/routes/courses/show.js
+++ app/routes/courses/show.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
     model({slug}){
         const { courses } = this.modelFor('application');
-        const matchedCourse = courses.filter(course => course.attributes.slug === slug);
-        return matchedCourse.length > 0 ? matchedCourse[0].attributes.title : 'No item matched';
+        const matchedCourse = courses.filter(course => course.id === slug);
+        return matchedCourse.length > 0 ? matchedCourse[0].title : 'No item matched';
     }
 });
